3ab3bf26ece39a1809d7972aa0b978d51f425217
Devanshu Tiwari <dev.spectre91@gmail.com>
2021-07-28 04:58:14 +0530

    refactored to reset input

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,14 @@ import React from "react";
 import Activity from "./Activity";
 import dayArray from "../data";
 
-
-
 class App extends React.Component {
   constructor() {
     super();
 
     //defining state
     this.state = {
-     
       activities: [],
-      inputValue:"",
+      inputValue: "",
     };
     //binding methods
     this.handleChange = this.handleChange.bind(this);
@@ -22,7 +19,7 @@ class App extends React.Component {
   handleToggle(id, name) {
     const activities = JSON.parse(JSON.stringify(this.state.activities));
     activities.forEach((activity) => {
-      if (activity.name=== name) {
+      if (activity.name === name) {
         activity.days.forEach((day) => {
           if (day.id === id) {
             day.isDone = !day.isDone;
@@ -35,36 +32,41 @@ class App extends React.Component {
     });
   }
   handleChange(e) {
-
     this.setState({
       inputValue: e.target.value,
     });
   }
   handleClick(e, index) {
-    let activityArray= [...this.state.activities];
+    let activityArray = [...this.state.activities];
     console.log(activityArray);
-    if(e.target.name==="delete"){
-       activityArray.splice(index, 1); 
-         this.setState({
-           activities:[...activityArray] 
-         })
-    return}
+    if (e.target.name === "delete") {
+      activityArray.splice(index, 1);
+      this.setState({
+        activities: [...activityArray],
+      });
+      return;
+    }
 
-
-    this.setState((prevState) => {
-      return {
-        activities: prevState.activities.concat([
-          {
-            name: this.state.inputValue,
-            days: [...dayArray],
-          },
-        ]),
-      };
-    });
+    if (this.state.inputValue) {
+      this.setState(
+        (prevState) => {
+          return {
+            activities: prevState.activities.concat([
+              {
+                name: this.state.inputValue,
+                days: [...dayArray],
+              },
+            ]),
+          };
+        },
+        () => {
+          this.setState({ inputValue: "" });
+        }
+      );
+    }
   }
 
   render() {
-    
     return (
       <div className="p-6">
         <h1 className=" has-text-centered title has-text-info">
@@ -104,9 +106,14 @@ class App extends React.Component {
             </p>
           </div> */}
         </div>
-        {this.state.activities.map((activity,index) => {
+        {this.state.activities.map((activity, index) => {
           return (
-            <Activity details={activity} index={index} handleToggle={this.handleToggle} handleDelete={this.handleClick} />
+            <Activity
+              details={activity}
+              index={index}
+              handleToggle={this.handleToggle}
+              handleDelete={this.handleClick}
+            />
           );
         })}
       </div>
