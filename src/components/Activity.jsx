fbc227630b0d74fa55ebd5034a18c7230c852979
Devanshu Tiwari <dev.spectre91@gmail.com>
2021-07-28 04:41:39 +0530

    Activity Tracker

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -1,50 +1,27 @@
 import React from 'react';
 
+
 class Activity extends React.Component{
     constructor(props){
        super(props);
 
     }
-
+   
     render(){
         return <>
         <div className='box mt-4 p-2 columns activity'>
         
               <div className='column has-background-info-light is-3'>
-               <h2 className='is-size-4 mt-6 has-text-weight-bold has-text-centered'>Music</h2>
+               <h2 className='is-size-4 mt-6 has-text-weight-bold has-text-centered'>{this.props.details.name}</h2>
               </div>
               <div className='columns ml-6 mt-2 is-multiline'>
-                  <button className='column p-1 day is-1 button'>1</button>
-                  <button className='column p-1 day is-1 button'>2</button>
-                  <button className='column p-1 day is-1 button'>3</button>
-                  <button className='column p-1 day is-1 button'>4</button>
-                  <button className='column p-1 day is-1 button'>5</button>
-                  <button className='column p-1 day is-1 button'>6</button>
-                  <button className='column p-1 day is-1 button'>7</button>
-                  <button className='column p-1 day is-1 button'>1</button>
-                  <button className='column p-1 day is-1 button'>2</button>
-                  <button className='column p-1 day is-1 button'>3</button>
-                  <button className='column p-1 day is-1 button'>4</button>
-                  <button className='column p-1 day is-1 button'>5</button>
-                  <button className='column p-1 day is-1 button'>6</button>
-                  <button className='column p-1 day is-1 button'>7</button>
-                  <button className='column p-1 day is-1 button'>1</button>
-                  <button className='column p-1 day is-1 button'>2</button>
-                  <button className='column p-1 day is-1 button'>3</button>
-                  <button className='column p-1 day is-1 button'>4</button>
-                  <button className='column p-1 day is-1 button'>5</button>
-                  <button className='column p-1 day is-1 button'>6</button>
-                  <button className='column p-1 day is-1 button'>7</button>
-                  <button className='column p-1 day is-1 button'>1</button>
-                  <button className='column p-1 day is-1 button'>2</button>
-                  <button className='column p-1 day is-1 button'>3</button>
-                  <button className='column p-1 day is-1 button'>4</button>
-                  <button className='column p-1 day is-1 button'>5</button>
-                  <button className='column p-1 day is-1 button'>6</button>
-                  <button className='column p-1 day is-1 button'>7</button>
+                 {this.props.details.days.map(day=>{
+                     return <button onClick={()=>{this.props.handleToggle(day.id, this.props.details.name)}} className={day.isDone?'column p-1 day is-1 button has-background-primary':'column p-1 day is-1 button'}>{day.id}</button>
+                 })}
+                  
                   
               </div>
-              <button class="delete is-medium has-background-danger"></button>
+              <button name='delete' onClick={(e)=>{this.props.handleDelete(e, this.props.index)}} className="delete is-medium has-background-danger"></button>
         </div>
         
         </>
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,70 @@
 import React from "react";
 import Activity from "./Activity";
+import dayArray from "../data";
+
+
 
 class App extends React.Component {
   constructor() {
     super();
+
+    //defining state
     this.state = {
-      month: null,
+     
+      activities: [],
+      inputValue:"",
     };
+    //binding methods
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleToggle = this.handleToggle.bind(this);
+  }
+  handleToggle(id, name) {
+    const activities = JSON.parse(JSON.stringify(this.state.activities));
+    activities.forEach((activity) => {
+      if (activity.name=== name) {
+        activity.days.forEach((day) => {
+          if (day.id === id) {
+            day.isDone = !day.isDone;
+          }
+        });
+      }
+    });
+    this.setState({
+      activities: activities,
+    });
+  }
+  handleChange(e) {
+
+    this.setState({
+      inputValue: e.target.value,
+    });
+  }
+  handleClick(e, index) {
+    let activityArray= [...this.state.activities];
+    console.log(activityArray);
+    if(e.target.name==="delete"){
+       activityArray.splice(index, 1); 
+         this.setState({
+           activities:[...activityArray] 
+         })
+    return}
+
+
+    this.setState((prevState) => {
+      return {
+        activities: prevState.activities.concat([
+          {
+            name: this.state.inputValue,
+            days: [...dayArray],
+          },
+        ]),
+      };
+    });
   }
 
   render() {
+    
     return (
       <div className="p-6">
         <h1 className=" has-text-centered title has-text-info">
@@ -23,16 +78,23 @@ class App extends React.Component {
                   className="input px-6"
                   type="text"
                   name="activity"
+                  onChange={this.handleChange}
+                  value={this.state.inputValue}
                   placeholder="e.g. coding"
                 />
               </p>
 
               <p className="control">
-                <button className="button is-primary">Add Activity</button>
+                <button
+                  className="button is-primary"
+                  onClick={this.handleClick}
+                >
+                  Add Activity
+                </button>
               </p>
             </div>
           </div>
-          <div className="level-right">
+          {/* <div className="level-right">
             <p className="control">
               <div className="select">
                 <select>
@@ -40,11 +102,13 @@ class App extends React.Component {
                 </select>
               </div>
             </p>
-          </div>
+          </div> */}
         </div>
-        <Activity />
-        <Activity />
-        <Activity />
+        {this.state.activities.map((activity,index) => {
+          return (
+            <Activity details={activity} index={index} handleToggle={this.handleToggle} handleDelete={this.handleClick} />
+          );
+        })}
       </div>
     );
   }
